6aace3c306bd12b21e6d028c8623cbf3c1858085
Margarita D <margaritdrozdoff@gmail.com>
2018-01-29 09:56:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,3 +20,14 @@ function destructivelyRemoveLastKitten(){
 function destructivelyRemoveFirstKitten(){
   kittens.shift()
 }
+
+function appendKitten(name){
+  var newKittens = [...kittens,name]
+  return newKittens
+}
+
+function prependKitten(name){
+  var newKittens = [name, ...kittens]
+  return newKittens
+}
+
